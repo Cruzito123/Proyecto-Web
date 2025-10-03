4c595b3aa46e18468ee03a6426dc22157a4cdad4
Martin CA <martincruzarmas@gmail.com>
2025-10-02 22:18:40 -0600

    Cambios generales

--- Restaurant/myRestaurant/views.py
+++ Restaurant/myRestaurant/views.py
@@ -3,7 +3,9 @@ from django.db import connection
 from .models import Platillo
 
 def Inicio(request):
-    return render(request, 'index.html')
+    # Obtenemos todos los platillos de la base de datos
+    platillos = Platillo.objects.all()
+    return render(request, 'index.html', {'platillos': platillos})
 
 def Altas(request):
     return render(request, 'Altas.html')
