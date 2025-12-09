85b3c4d4cb371654037370aa2fe651692e6c87b5
Martin CA <martincruzarmas@gmail.com>
2025-12-08 22:13:27 -0600

    Preparando para produccion

--- Restaurant/manage.py
+++ Restaurant/manage.py
@@ -3,23 +3,24 @@
 import os
 import sys
 import ssl
-# 👇 2. Pega este bloque completo para arreglar el error en Python 3.12/3.13
-if not hasattr(ssl, 'wrap_socket'):
-    def wrap_socket(sock, keyfile=None, certfile=None, **kwargs):
-        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-        # Cargar el protocolo correcto si se especifica, o usar TLS por defecto
-        if 'ssl_version' in kwargs:
-            context = ssl.SSLContext(kwargs['ssl_version'])
-        
-        if certfile:
-            context.load_cert_chain(certfile=certfile, keyfile=keyfile)
-            
-        return context.wrap_socket(sock, server_side=kwargs.get('server_side', False))
-    
-    ssl.wrap_socket = wrap_socket
+
 # 👆 Fin del arreglo
 
 def main():
+        # 👇 2. Pega este bloque completo para arreglar el error en Python 3.12/3.13
+    if not hasattr(ssl, 'wrap_socket'):
+        def wrap_socket(sock, keyfile=None, certfile=None, **kwargs):
+            context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+            # Cargar el protocolo correcto si se especifica, o usar TLS por defecto
+            if 'ssl_version' in kwargs:
+                context = ssl.SSLContext(kwargs['ssl_version'])
+            
+            if certfile:
+                context.load_cert_chain(certfile=certfile, keyfile=keyfile)
+                
+            return context.wrap_socket(sock, server_side=kwargs.get('server_side', False))
+        
+        ssl.wrap_socket = wrap_socket
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Restaurant.settings')
     try:
